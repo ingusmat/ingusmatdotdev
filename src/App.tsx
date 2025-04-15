a0ed34b981757d6fbe57d7584499f778ca720e58
Ingus Mat Burleson <ingusmat@gmail.com>
2025-04-14 22:15:52 -0400

    First Changes - remove example homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 import { Mail, Github, Linkedin } from "lucide-react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
